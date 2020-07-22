64bd3d278a643a3799ce3a737034bb984f5b19f6
FBUENOD <fernandobuenodansi@gmail.com>
2020-07-22 19:20:34 -0300

    update

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,48 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-navigation-drawer v-model="drawer" app clipped>
+      <v-list dense>
+        <v-list-item :key="i" v-for="(route, i) in $router.options.routes" link :to="route.path">
+          <v-list-item-content>
+            <v-list-item-title>{{ route.name }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+
+    <v-app-bar app clipped-left>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-toolbar-title>InstaLite</v-toolbar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container fluid>
+        <v-row>
+          <v-col>
+            <router-view />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+
+    <v-footer app>
+      <span>&copy; 2020 | InstaLite</span>
+    </v-footer>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+export default {
+  props: {
+    source: String
+  },
 
-#nav {
-  padding: 30px;
+  data: () => ({
+    drawer: null
+  }),
 
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+  created() {
+    this.$vuetify.theme.dark = false;
   }
-}
-</style>
+};
+</script>
